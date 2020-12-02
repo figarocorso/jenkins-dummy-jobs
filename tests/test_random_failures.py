d4c6776a5b8402c23a1e14f23624601d085a970d
Miguel Julián <figarocorso@users.noreply.github.com>
2020-12-02 23:07:53 +0100

    Switch randomness every hour (#2)
    * Add random delay once every hour
    
    * Pass random failures (sometimes) once every two hours

--- tests/test_random_failures.py
+++ tests/test_random_failures.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from random import randint
 
 import unittest
@@ -5,35 +6,42 @@ import unittest
 
 class RandomFailures(unittest.TestCase):
 
+    def __init__(self, *args, **kwargs):
+        super(RandomFailures, self).__init__(*args, **kwargs)
+        self.stable = datetime.now().hour % 2
+
     def test_1(self):
-        self.assertEqual(1, randint(1, 3))
+        self._assert_with_stable_condition()
 
     def test_2(self):
-        self.assertEqual(1, randint(1, 3))
+        self._assert_with_stable_condition()
 
     def test_3(self):
-        self.assertEqual(1, randint(1, 3))
+        self._assert_with_stable_condition()
 
     def test_4(self):
-        self.assertEqual(1, randint(1, 3))
+        self._assert_with_stable_condition()
 
     def test_5(self):
-        self.assertEqual(1, randint(1, 3))
+        self._assert_with_stable_condition()
 
     def test_6(self):
-        self.assertEqual(1, randint(1, 3))
+        self._assert_with_stable_condition()
 
     def test_7(self):
-        self.assertEqual(1, randint(1, 3))
+        self._assert_with_stable_condition()
 
     def test_8(self):
-        self.assertEqual(1, randint(1, 3))
+        self._assert_with_stable_condition()
 
     def test_9(self):
+        self._assert_with_stable_condition()
+
+    def test_10_fail_one_out_of_three(self):
         self.assertEqual(1, randint(1, 3))
 
-    def test_10(self):
-        self.assertEqual(1, randint(1, 3))
+    def _assert_with_stable_condition(self):
+        self.assertEqual(1, 1 if self.stable else randint(1, 3))
 
 
 if __name__ == "__main__":
