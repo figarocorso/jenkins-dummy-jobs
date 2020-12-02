dbc025e07781ff585061418467b5d04dc889f785
figarocorso <figarocorso@gmail.com>
2020-12-02 23:02:54 +0100

    Add random delay once every hour

--- tests/test_all_right.py
+++ tests/test_all_right.py
@@ -1,31 +1,44 @@
+from datetime import datetime
+from random import randint
+from time import sleep
+
 import unittest
 
 
 class AllRight(unittest.TestCase):
 
+    def __init__(self, *args, **kwargs):
+        super(AllRight, self).__init__(*args, **kwargs)
+        self.shall_sleep = datetime.now().hour % 2
+
     def test_1(self):
-        self.assertEqual(1, 1)
+        self._assert_and_sleep(1)
 
     def test_2(self):
-        self.assertEqual(2, 2)
+        self._assert_and_sleep(2)
 
     def test_3(self):
-        self.assertEqual(3, 3)
+        self._assert_and_sleep(3)
 
     def test_4(self):
-        self.assertEqual(4, 4)
+        self._assert_and_sleep(4)
 
     def test_5(self):
-        self.assertEqual(5, 5)
+        self._assert_and_sleep(5)
 
     def test_6(self):
-        self.assertEqual(6, 6)
+        self._assert_and_sleep(6)
 
     def test_7(self):
-        self.assertEqual(7, 7)
+        self._assert_and_sleep(7)
 
     def test_8(self):
-        self.assertEqual(8, 8)
+        self._assert_and_sleep(8)
+
+    def _assert_and_sleep(self, test_number):
+        if self.shall_sleep:
+            sleep(randint(1, test_number))
+        self.assertEqual(test_number, test_number)
 
 
 if __name__ == "__main__":
